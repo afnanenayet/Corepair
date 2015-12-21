40acbabb4ccdcfe3fafa35fa2919e8a6f1213fef
Afnan Enayet <afnan.enayet@gmail.com>
2015-12-21 15:30:06 -0500

    updated UI

--- ViewController.swift
+++ ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: NSViewController {
     @IBOutlet weak var verifyButton: NSButton!
     @IBOutlet weak var repairButton: NSButton!
     @IBOutlet weak var diskOption: NSMatrix!
-    
+    @IBOutlet weak var pathSelection: NSPathControl!
+    @IBOutlet weak var cellSelection: NSPathCell!
     
     override func viewDidAppear() {
         self.view.window!.title = "Repair Permissions"
@@ -26,9 +27,11 @@ class ViewController: NSViewController {
     
     @IBAction func verifyClicked(sender: AnyObject) {
         print("Verify button clicked") // DEBUG
-        
+        print(String(cellSelection.URL))
+        var urlString = String(cellSelection.URL)
         let shellInterface = clUtil()
-        tStore.textStorage!.mutableString.setString(shellInterface.diskCommand("/", verify: true))
+        //tStore.textStorage!.mutableString.setString(shellInterface.diskCommand("/", verify: true))
+        tStore.textStorage!.mutableString.setString(shellInterface.diskCommand(String(cellSelection.URL)))
     }
     
     @IBAction func repairClicked(sender: AnyObject) {
@@ -37,6 +40,18 @@ class ViewController: NSViewController {
         let shellInterface = clUtil()
         // TODO change verify to false
         tStore.textStorage!.mutableString.setString(shellInterface.diskCommand("/", verify: true))
+    }
+    
+    func getFileDir() -> String {
+        let index = diskOption.selectedRow
+        
+        if index == 0 {
+            return "/"
+        }
+        
+        else {
+            return "/"
+        }
     }
 }
 
